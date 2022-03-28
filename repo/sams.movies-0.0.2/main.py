a9f69ef81571c11a7affb173592ad2592c2238cf
Sam Greaves <sam.greaves@bglgroup.co.uk>
2022-03-28 22:38:32 +0100

    get link to channel

--- repo/sams.movies-0.0.2/main.py
+++ repo/sams.movies-0.0.2/main.py
@@ -11,10 +11,87 @@ import xbmcgui
 import xbmcplugin
 import requests
 
+from __future__ import unicode_literals
+
+import os
+from base64 import b64encode, b64decode
+from binascii import a2b_hex
+from Cryptodome.Cipher import PKCS1_v1_5 as Cipher_PKCS1_v1_5
+from Cryptodome.Cipher import DES
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Util.Padding import unpad
+from requests.packages.urllib3.util import Retry
+from requests.adapters import HTTPAdapter
+
 # Get the plugin url in plugin:// notation.
 _url = sys.argv[0]
 # Get the plugin handle as an integer number.
 _handle = int(sys.argv[1])
+
+base_url = "https://rocktalk.net/tv/index.php"  # 31.220.0.210 31.220.0.8
+user_agent = "Dalvik/2.1.0 (Linux; U; Android 5.1.1; AFTS Build/LVY48F)"
+player_user_agent = "mediaPlayerhttp/2.5 (Linux;Android 5.1) ExoPlayerLib/2.6.1"
+s = requests.Session()
+s.headers.update({"User-Agent": "USER-AGENT-tvtap-APP-V2"})
+
+def payload():
+    _pubkey = RSA.importKey(
+        a2b_hex(
+            "30819f300d06092a864886f70d010101050003818d003081890281"
+            "8100bfa5514aa0550688ffde568fd95ac9130fcdd8825bdecc46f1"
+            "8f6c6b440c3685cc52ca03111509e262dba482d80e977a938493ae"
+            "aa716818efe41b84e71a0d84cc64ad902e46dbea2ec61071958826"
+            "4093e20afc589685c08f2d2ae70310b92c04f9b4c27d79c8b5dbb9"
+            "bd8f2003ab6a251d25f40df08b1c1588a4380a1ce8030203010001"
+        )
+    )
+    _msg = a2b_hex(
+        "7b224d4435223a22695757786f45684237686167747948392b58563052513d3d5c6e222c22534"
+        "84131223a2242577761737941713841327678435c2f5450594a74434a4a544a66593d5c6e227d"
+    )
+    cipher = Cipher_PKCS1_v1_5.new(_pubkey)
+    return b64encode(cipher.encrypt(_msg))
+
+def api_request(case, channel_id=None):
+    headers = {"app-token": "37a6259cc0c1dae299a7866489dff0bd"}
+    data = {"payload": payload(), "username": "603803577"}
+    if channel_id:
+        data["channel_id"] = channel_id
+    params = {"case": case}
+    r = s.post(base_url, headers=headers, params=params, data=data, timeout=5, verify = False)
+    r.raise_for_status()
+    resp = r.json()
+    if resp["success"] == 1:
+        return resp["msg"]
+    else:
+        raise ValueError(resp["msg"])
+
+def update_channels():
+    _channels = api_request("get_all_channels")["channels"]
+    _category_list = []
+    _categories = []
+    for c in _channels:
+        if c.get("cat_id") not in _category_list:
+            _category_list.append(c.get("cat_id"))
+            _categories.append({"cat_id": c.get("cat_id"), "cat_name": c.get("cat_name")})
+    print(_channels)
+
+def get_channel_links(pk_id):
+    _channel = api_request("get_channel_link_with_token_latest", pk_id)["channel"][0]
+    links = []
+    for stream in _channel.keys():
+        if "stream" in stream or "chrome_cast" in stream:
+            _crypt_link = _channel[stream]
+            if _crypt_link:
+                d = DES.new(b"98221122", DES.MODE_ECB)
+                link = unpad(d.decrypt(b64decode(_crypt_link)), 8).decode("utf-8")
+                if not link == "dummytext" and link not in links:
+                    links.append(link)
+                    print(link)
+    return links
+
+update_channels()
+link = get_channel_links(77)
 
 r =requests.get('http://samgreaves.com:3020/videos/kodi')
 VIDEOS = r.json()
@@ -68,8 +145,9 @@ VIDEOS.append({
         {
             'name': 'Test',
             'thumb': '',
-            'video': 'http://stream.tvtap.live:8081/live/uk-amc.stream/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9My8yOC8yMDIyIDg6MTA6NTkgUE0maGFzaF92YWx1ZT1kalRDWGFrZnh1Yk5WUXVRQXI4czVnPT0mdmFsaWRtaW51dGVzPTIw',
+            'video': link[0]
             'genre': 'Entertainment'
+
         }
     ]
 })
@@ -271,7 +349,6 @@ def router(paramstring):
         # If the plugin is called from Kodi UI without any parameters,
         # display the list of video categories
         list_categories()
-
 
 if __name__ == '__main__':
     # Call the router function and pass the plugin call parameters to it.
