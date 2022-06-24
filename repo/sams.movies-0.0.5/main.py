d3adbc66ebf7988526970392be98961809ebc252
Sam Greaves <sam.greaves@bglgroup.co.uk>
2022-06-24 19:48:56 +0100

    Update version number and add username to header on video play request

--- repo/sams.movies-0.0.5/main.py
+++ repo/sams.movies-0.0.5/main.py
@@ -30,6 +30,7 @@ _handle = int(sys.argv[1])
 
 base_url = "https://rocktalk.net/tv/index.php"  # 31.220.0.210 31.220.0.8
 user_agent = "Dalvik/2.1.0 (Linux; U; Android 5.1.1; AFTS Build/LVY48F)"
+username = "Sam"
 player_user_agent = "mediaPlayerhttp/2.5 (Linux;Android 5.1) ExoPlayerLib/2.6.1"
 s = requests.Session()
 s.headers.update({"User-Agent": "USER-AGENT-tvtap-APP-V2"})
@@ -319,6 +320,7 @@ def play_video(path):
     """
     # Create a playable item with a path to play.
     play_item = xbmcgui.ListItem(path=path)
+    play_item.setProperty("username",username)
     # Pass the item to the Kodi player.
     xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
 
